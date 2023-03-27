8a491ffbf2cd21179bcc04b098ab0e2a450f135c
2530470001 <2530470001@qq.com>
2023-03-27 17:54:37 +0800

    代码嵌入

---
+++ assemble-platform-baiduidgen/src/main/java/com/coca/assesmble/baiduidgen/UidGenerator.java
@@ -0,0 +1,24 @@
+package com.coca.assesmble.baiduidgen;
+
+import com.coca.assesmble.baiduidgen.exception.UidGenerateException;
+
+public interface UidGenerator {
+
+    /**
+     * Get a unique ID
+     *
+     * @return UID
+     * @throws UidGenerateException
+     */
+    long getUID() throws UidGenerateException;
+
+    /**
+     * Parse the UID into elements which are used to generate the UID. <br>
+     * Such as timestamp & workerId & sequence...
+     *
+     * @param uid
+     * @return Parsed info
+     */
+    String parseUID(long uid);
+
+}
---
+++ assemble-platform-uuidKey/src/test/java/com/coca/assemble/uuidkey/AssemblePlatformUuidKeyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coca.assemble.uuidkey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssemblePlatformUuidKeyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
